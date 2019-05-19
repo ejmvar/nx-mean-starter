1f86946aff7b194b3519ed15bbe7dfa6222cf772
Damian Bielecki <bielik20@gmail.com>
2019-05-19 15:03:47 +0200

    Add nestjs backend

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,3 +1,4 @@
-import { bootstrap } from '@nx-mean-starter/backend/express';
+// import { bootstrap } from '@nx-mean-starter/backend/express';
+import { bootstrap } from '@nx-mean-starter/backend/nestjs';
 
 export default bootstrap();
